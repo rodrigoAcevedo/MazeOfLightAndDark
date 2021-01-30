0090dedaa1516f1a9d6ad8c28754e0ae3f495365
Rodrigo Acevedo <rodriacvdo@gmail.com>
2021-01-30 00:43:42 -0300

    First version of fog of war
    Fog of war

---
+++ Assets/TileFogOfWar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileFogOfWar : MonoBehaviour
+{
+
+    private Color darkTileColor = new Color(0f, 0f, 0f, 1f);
+
+    private Tile[] childrenTiles;
+    public List<GameObject> tiles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        childrenTiles = GetComponentsInChildren<Tile>();
+
+        foreach(Tile tile in childrenTiles)
+        {
+            tiles.Add(tile.gameObject);
+        }
+
+        // Set to dark
+        foreach(GameObject tile in tiles)
+        {
+            tile.GetComponent<SpriteRenderer>().color = darkTileColor;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
