71974f839f98f8254261a65ab7addc5999a97219
ArazardStreight <sonaglioni.enoc@gmail.com>
2021-01-30 15:17:22 -0300

    Pickup Objects
    Ahora se puede agarrar una llave y aparece en la UI

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+
+    public bool key;
+    public UnityEngine.UI.Image keyIcon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        key = false;
+        keyIcon.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
