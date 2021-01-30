e64e5c7b993aba057428f002807d2558df81e129
Rodrigo Acevedo <rodriacvdo@gmail.com>
2021-01-30 20:53:58 -0300

    Fog of war on Tilemap

--- Assets/Scripts/PlayerFogOfWar.cs
+++ Assets/Scripts/PlayerFogOfWar.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class PlayerFogOfWar : MonoBehaviour
 {
-    private RaycastHit2D[] hit;
+    private List<Vector3Int> hit;
     private int frames;
     private LayerMask scenarioLayerMask;
+    public Tilemap fogOfWar;
 
     [SerializeField]
     private float innerRadius;
+    [SerializeField]
+    private Color grayCellColor;
+    [SerializeField]
+    private Color lightCellColor;
 
     private void Awake()
     {
         frames = 0;
         scenarioLayerMask = LayerMask.GetMask("Scenario");
+
+        grayCellColor = new Color(1f, 1f, 1f, 0.5f);
+        lightCellColor = new Color(1f, 1f, 1f, 0f);
+
+        foreach (Vector3Int tilePosition in fogOfWar.cellBounds.allPositionsWithin)
+        {
+            fogOfWar.RemoveTileFlags(tilePosition, TileFlags.LockColor);
+
+        }
+
     }
 
 
@@ -25,20 +41,54 @@ public class PlayerFogOfWar : MonoBehaviour
         {
             frames = 0;
 
-            hit = Physics2D.CircleCastAll(transform.position, 2f, new Vector2(0, 0), 3f, scenarioLayerMask);
+            hit = TileCircleCast(transform.position);
 
-            foreach (var tile in hit)
+            if (hit.Count > 0)
             {
-                Vector3 dis = transform.position - tile.collider.transform.position;
-                if (dis.x > innerRadius || dis.x < -innerRadius || dis.y > innerRadius || dis.y < -innerRadius)
+                foreach (Vector3Int tile in hit)
                 {
-                    tile.collider.transform.gameObject.GetComponent<SpriteRenderer>().color = new Color(.5f, .5f, .5f, 1.0f);
-                }
-                else
-                {
-                    tile.collider.transform.gameObject.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1.0f);
+                    Vector3 dis = transform.position - (Vector3)tile;
+                    if (dis.x > innerRadius || dis.x < -innerRadius || dis.y > innerRadius || dis.y < -innerRadius)
+                    {
+                        fogOfWar.SetColor(tile, grayCellColor);
+                    }
+                    else
+                    {
+                        fogOfWar.SetColor(tile, lightCellColor);
+                    }
                 }
             }
         }
+    }
+
+    List<Vector3Int> TileCircleCast(Vector3 pos)
+    {
+        int radius = 2;
+        pos.x = Mathf.CeilToInt(pos.x);
+        pos.y = Mathf.CeilToInt(pos.y);
+
+        int leftOffset = (int)Mathf.Floor(pos.x - radius);
+        int rightOffset = (int)Mathf.Ceil(pos.x + radius);
+        int topOffset = (int)Mathf.Floor(pos.y - radius);
+        int bottomOffset =  (int)Mathf.Ceil(pos.y + radius);
+
+        List<Vector3Int> tiles = new List<Vector3Int>();
+
+        for (int x = leftOffset; x < rightOffset; x++)
+        {
+            for (int y = topOffset; y < bottomOffset; y++)
+            {
+                Vector3Int tilePosition = Vector3Int.FloorToInt(new Vector3(x, y, 0f));
+                if (fogOfWar.GetTile(tilePosition) != null 
+                    /*&& !(x == leftOffset && y == topOffset) // Esquina SUROESTE
+                    && !(x == rightOffset && y == topOffset) // Esquina SURESTE
+                    && !(x == leftOffset && y == bottomOffset) // Esquina NOROESTE
+                    && !(x == rightOffset && y == bottomOffset)*/) // Esquina NORESTE
+                {
+                    tiles.Add(tilePosition);
+                }
+            }
+        }
+        return tiles;
     }
 }
