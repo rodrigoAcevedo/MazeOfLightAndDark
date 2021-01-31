e279abcbd9843208687a7d428aa5cdeeacca7b10
ArazardStreight <sonaglioni.enoc@gmail.com>
2021-01-31 16:58:56 -0300

    Life
    Se agrega vida al jugador

---
+++ Assets/HealthPlayer.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public HealthManager healthManager;
+    public int life;
+    public float invencibleTime;
+    private float invencibleTimeReference;
+    public NavigationManager navMng;
+
+    private bool canGetHurt;
+    void Start()
+    {
+        invencibleTimeReference = invencibleTime;
+        canGetHurt = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetHearth();
+        }
+        canGetHurt = LifeCooldown();
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        if(collision.gameObject.tag == "dangerous" && collision.gameObject.GetComponent<TrapBehavior>().GetState())
+        {
+            GetHearth();
+        }
+    }
+
+    private void GetHearth()
+    {
+        if (canGetHurt)
+        {
+            life -= 1;
+            healthManager.ChangeLifeIcon(life);
+            canGetHurt = false;
+            invencibleTime = invencibleTimeReference;
+            if(life <= 0)
+            {
+                navMng.StartGame();
+            }
+        }
+    }
+
+    private bool LifeCooldown()
+    {
+        invencibleTime -= 0.1f;
+        if(invencibleTime <= 0.0f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
---
+++ Assets/Scripts/TrapBehavior.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrapBehavior : MonoBehaviour
+{
+    public Sprite[] trapSprites;
+    public float timeActived;
+    public float timeDeactived;
+
+    private float timerActivedReference;
+    private float timerDeactivedReference;
+
+    private bool isActived;
+    // Start is called before the first frame update
+    void Start()
+    {
+          timerActivedReference = timeActived;
+          timerDeactivedReference = timeDeactived;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        TrapTimer();
+    }
+
+    private void TrapTimer()
+    {
+        if(isActived == false)
+        {
+            timeActived -= 0.1f;
+            if (timeActived <= 0.0f)
+            {
+                timeActived = timerActivedReference;
+                isActived = true;
+                GetComponent<SpriteRenderer>().sprite = trapSprites[0];
+            }
+        }
+        else
+        {
+            timeDeactived -= 0.1f;
+            if (timeDeactived <= 0.0f)
+            {
+                timeDeactived = timerDeactivedReference;
+                isActived = false;
+                GetComponent<SpriteRenderer>().sprite = trapSprites[1];
+            }
+        }
+
+    }
+
+    public bool GetState()
+    {
+        return isActived;
+    }
+}
